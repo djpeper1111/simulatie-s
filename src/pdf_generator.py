3d18a482f1eb62869ac3c05048a5b2198d4f5844
djpeper1111 <djenspierings@gmail.com>
2026-06-11 15:00:17 +0200

    feat:: added import

---
+++ src/pdf_generator.py
@@ -0,0 +1,9 @@
+#import van libarys
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+from src.Merwede_Model import generate_graphs
+
+
+
