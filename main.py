df2c4351171a3520317015ffda65d924b0d532fe
djpeper1111 <djenspierings@gmail.com>
2026-06-11 16:22:48 +0200

    new: add functions

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ Created on Thu Jun 11 13:47:58 2026
 
 @author: Luuk
 """
-
+#import 
 from src.Merwede_Model import generate_graphs, get_profile_df
-
+from pdf_generator import generate_pdf
 from pathlib import Path
 
 
 ev_count = 200
 profiles_folder = Path("data/Merwede_Profiles")
 
-def main():
-    generate_graphs(
+
+def generate_graphs_only():
+    graphs = generate_graphs(
         get_profile_df(ev_count, profiles_folder)
     )
-    
-    
+    return graphs
+
+def generate_pdf_report():
+    generate_pdf()
+
+def compleet():
+    generate_graphs_only()
+    generate_pdf_report()
+
 if __name__ == "__main__":
-    main()
+    pass
