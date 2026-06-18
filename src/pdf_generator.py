d9ad3e22d3d598e31ac74e20444a5393d910981a
djpeper1111 <djenspierings@gmail.com>
2026-06-18 12:11:25 +0200

    update: name, pdf file.

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -2,21 +2,49 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import os
 
-def generate_pdf(graphs):
-    output_map = "output_pdf"
+def generate_pdf(graphs, ev_count, cos_phi):
+    output_map = "output"
     os.makedirs(output_map, exist_ok=True)
-    pdf_path = os.path.join(output_map, "Merwede_Profiles.pdf")
 
-    with PdfPages(pdf_path) as pdf:
-        fig = plt.figure(figsize=(10, 6))
-        plt.text(0.5, 0.5, "Merwede energieprofiel Rapport",
-                 ha='center', va='center', fontsize=28)
-        pdf.savefig(fig)
-        plt.close(fig)
+    for profile_name, graph in graphs.items():
+        print(f"Generating PDF for {profile_name}...")
 
-        for profile_name, graph_dict in graphs.items():
-            for graph_name, fig in graph_dict.items():
-                pdf.savefig(fig)
+        filename = f"{profile_name}_EvCount-{ev_count}_CosPhi-{cos_phi}.pdf"
+        pdf_path = os.path.join(output_map, filename)
+
+        with PdfPages(pdf_path) as pdf:
+            for graph_name, fig in graph.items():
+
+                temp_path = "temp_fig.png"
+                fig.savefig(temp_path, dpi=300, bbox_inches="tight")
                 plt.close(fig)
 
-    print("PDF opgeslagen in:", pdf_path)
+                img = plt.imread(temp_path)
+                img_h, img_w = img.shape[0], img.shape[1]
+                img_ratio = img_w / img_h
+
+                a4_w, a4_h = 8.27, 11.69
+                a4_ratio = a4_w / a4_h
+
+                a4_fig = plt.figure(figsize=(a4_w, a4_h))
+
+                if img_ratio > a4_ratio:
+                    display_w = 0.9
+                    display_h = display_w / img_ratio
+                else:
+                    display_h = 0.9
+                    display_w = display_h * img_ratio
+
+                left = (1 - display_w) / 2
+                bottom = (1 - display_h) / 2
+
+                ax = a4_fig.add_axes([left, bottom, display_w, display_h])
+                ax.imshow(img)
+                ax.axis("off")
+
+                pdf.savefig(a4_fig)
+                plt.close(a4_fig)
+
+                os.remove(temp_path)
+
+        print("PDF OPGESLAGEN", pdf_path)
